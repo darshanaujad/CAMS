bf957f99f427ce107f1fde88134080a69b761071
Darshana Ujad <darshana.ujad@gmail.com>
2026-03-09 18:50:16 +0530

    added

--- fe/src/pages/Login.jsx
+++ fe/src/pages/Login.jsx
@@ -86,7 +86,7 @@ export default function Login() {
 
     setLoading(true);
     try {
-      const res = await fetch(`http://localhost:5000/api/${loginUser}/login`, {
+      const res = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/api/${loginUser}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
