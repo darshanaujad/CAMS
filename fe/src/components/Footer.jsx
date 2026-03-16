8b00733b8e08db00c4c9031488c6cb4576ca34d6
Darshana Ujad <darshana.ujad@gmail.com>
2026-03-16 16:43:59 +0530

    added

--- fe/src/components/Footer.jsx
+++ fe/src/components/Footer.jsx
@@ -131,7 +131,7 @@ export default function Footer () {
           {/* Copyright */}
           <div className="border-t border-gray-800 pt-8 text-center">
             <p className="text-gray-400">
-              © 2024 College Academic Management System. All rights reserved.
+              © 2026 College Academic Management System. All rights reserved.
             </p>
           </div>
         </div>
